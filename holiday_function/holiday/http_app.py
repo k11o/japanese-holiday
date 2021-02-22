a761595fa80dad8dc9ea6bfb46fdd41fb0040996
Tatsuhiko Kono <konota@amazon.com>
2021-02-22 21:37:15 +0900

    #4 判定基準を間違えた結果、いつでもtrueを返すようになっていたので修正

--- holiday_function/holiday/http_app.py
+++ holiday_function/holiday/http_app.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
         try:
             target_date = datetime.datetime.strptime(
                 event['queryStringParameters'].get("date"), '%Y-%m-%d').date()
-            if len(table.query(KeyConditionExpression=Key('Date').eq(target_date.strftime("%Y-%m-%d")))) > 0:
+            if len(table.query(KeyConditionExpression=Key('Date').eq(target_date.strftime("%Y-%m-%d")))['Items']) > 0:
                 return {
                     "statusCode": 200,
                     "body": json.dumps({
