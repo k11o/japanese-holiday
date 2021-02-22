cbae14b770adb4d79ea42561b8eeef977e6a3f00
Tatsuhiko Kono <konota@amazon.com>
2021-02-22 21:06:53 +0900

    #4 WebAPIの完成　デプロイして動作確認も出来た

--- holiday_function/holiday/http_app.py
+++ holiday_function/holiday/http_app.py
@@ -1,7 +1,29 @@
+from boto3.dynamodb.conditions import Key
+import json
+import datetime
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
+
 def lambda_handler(event, context):
     table = dynamodb.Table('HolidayDB')
+    if event['queryStringParameters'] is not None:
+        try:
+            target_date = datetime.datetime.strptime(
+                event['queryStringParameters'].get("date"), '%Y-%m-%d').date()
+            if len(table.query(KeyConditionExpression=Key('Date').eq(target_date.strftime("%Y-%m-%d")))) > 0:
+                return {
+                    "statusCode": 200,
+                    "body": json.dumps({
+                        "result": "true",
+                    }),
+                }
+        except ValueError:
+            pass
 
-    return ""
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "result": "false",
+        }),
+    }
